cd2d9b452c2cc35d149f29fe3349d24e2f2547a6
arxangel008 <valera-bapbap56@mail.ru>
2016-04-07 18:53:02 +0700

    download main

--- src/ParseJson.groovy
+++ src/ParseJson.groovy
@@ -4,22 +4,49 @@ class ParseJson {
 
     public static void main(String[] args) {
 
-        def url = 'https://bower.herokuapp.com/packages/search/angular'
-        def file = new File('angular').newOutputStream()
-        file << new URL(url).openStream()
-        file.close()
+        def url = 'https://bower.herokuapp.com/packages/lookup/angular'.toURL().text
 
-        def jsonSlurper = new JsonSlurper()
-        def reader = new BufferedReader(new FileReader("angular"))
-        def parseData = jsonSlurper.parse(reader)
+        if (url.charAt(0) != "[")
+            url = '[' + url + ']'
+
+        def jsonSlurper = new JsonSlurper().parseText(url)
+        def parseData = jsonSlurper
         def urlList = new ArrayList<Item>()
         parseData.each {
             aUrl ->
-            url = new Item(aUrl.name, aUrl.url)
-            urlList.add(url)
+                url = new Item(aUrl.name, aUrl.url)
+                urlList.add(url)
         }
 
-        urlList.each {aUrl -> println aUrl}
+        urlList.each { aUrl -> println aUrl }
+
+        URL aURL = new URL(urlList[0].url)
+
+        def pathUrl = aURL.getPath().replaceAll("\\.git","")
+
+        def fileName = 'bower.json'
+        def gitHubUserContent = 'https://raw.githubusercontent.com'
+        def tag = '/master'
+
+        gitHubUserContent = gitHubUserContent + pathUrl + tag
+
+        url = (gitHubUserContent + '/' + fileName).toURL().text
+
+        parseData = new JsonSlurper().parseText(url)
+
+        String mainUrl = parseData.main
+
+        fileName = mainUrl.replaceAll("\\./","")
+
+        url = gitHubUserContent + '/' + fileName
+        def file = new File('main').newOutputStream()
+        file << new URL(url).openStream()
+        file.close()
+
+
+        println(pathUrl)
+
+
     }
 
 }
